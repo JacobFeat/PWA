bb82a1ead94477f6c1f0ef581630f4af8d54bd1f
JacobFeat <jakubk198@gmail.com>
2025-10-10 20:18:45 +0200

    flashcard init

--- src/app/app.ts
+++ src/app/app.ts
@@ -13,7 +13,7 @@ import { FlashcardService, Difficulty } from './flashcard.service';
 export class App {
   private flashcardService = inject(FlashcardService);
 
-  protected readonly title = signal('Flashcards App');
+  protected readonly title = signal('JavaScript Flashcards');
   protected readonly isFlipped = signal(false);
   protected readonly showButtons = signal(false);
 
